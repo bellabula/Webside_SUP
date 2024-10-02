31373bfd6b779d405a0f44e4fd3605cfa4abc652
bellabula <zz225864@gmail.com>
2024-10-02 22:18:00 +0800

    update partner page, add photo, modified layout

--- js/partner.js
+++ js/partner.js
@@ -1,3 +1,14 @@
+if (window.innerWidth < 768) {
+    document.querySelector("#pagehead h3").style.fontSize = "40px"
+    document.querySelector("#mainContent h1").style.fontSize = "40px"
+} else if (window.innerWidth < 1200) {
+    document.querySelector("#pagehead h3").style.fontSize = "53px"
+    document.querySelector("#mainContent h1").style.fontSize = "53px"
+} else {
+    document.querySelector("#pagehead h3").style.fontSize = `${window.innerWidth*0.044}px`
+    document.querySelector("#mainContent h1").style.fontSize = `${window.innerWidth*0.044}px`
+}
+
 document.querySelectorAll('figure').forEach(ele => {
     ele.firstElementChild.style.height = `${ele.firstElementChild.offsetWidth}px`
 })
@@ -6,4 +17,14 @@ window.onresize = () => {
     document.querySelectorAll('figure').forEach(ele => {
         ele.firstElementChild.style.height = `${ele.firstElementChild.offsetWidth}px`
     })
+    if (window.innerWidth < 768) {
+        document.querySelector("#pagehead h3").style.fontSize = "40px"
+        document.querySelector("#mainContent h1").style.fontSize = "40px"
+    } else if (window.innerWidth < 1200) {
+        document.querySelector("#pagehead h3").style.fontSize = "53px"
+        document.querySelector("#mainContent h1").style.fontSize = "53px"
+    } else {
+        document.querySelector("#pagehead h3").style.fontSize = `${window.innerWidth*0.044}px`
+        document.querySelector("#mainContent h1").style.fontSize = `${window.innerWidth*0.044}px`
+    }
 }
