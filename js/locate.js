6887f3938f85c8c497da705eed384b407897796c
bellabula <zz225864@gmail.com>
2024-10-03 22:38:19 +0800

    modified locate layout

--- js/locate.js
+++ js/locate.js
@@ -1,6 +1,7 @@
 if (window.innerWidth < 768) {
     document.querySelector("#mainContent iframe").classList.remove("set-middle")
     document.querySelector("#mainContent iframe").width = "100%"
+    document.querySelectorAll("#mainContent div")[0].style.fontSize = "64px"
 } else {
     document.querySelector("#mainContent iframe").classList.add("set-middle")
     document.querySelector("#mainContent iframe").width = "80%"
@@ -10,8 +11,10 @@ window.onresize = () => {
     if (window.innerWidth < 768) {
         document.querySelector("#mainContent iframe").classList.remove("set-middle")
         document.querySelector("#mainContent iframe").width = "100%"
+        document.querySelectorAll("#mainContent div")[0].style.fontSize = "64px"
     } else {
         document.querySelector("#mainContent iframe").classList.add("set-middle")
         document.querySelector("#mainContent iframe").width = "80%"
+        document.querySelectorAll("#mainContent div")[0].style.fontSize = "80px"
     }
 }
